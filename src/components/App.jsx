63a40864114d4b90b6ad1057de7547d9a2e63e9a
pawangit001 <103900468+pawangit001@users.noreply.github.com>
2022-05-19 22:37:27 +0530

    add map to display contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,22 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function App() {
+  function createcontact(contact) {
+    return (
+      <Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+      />
+    );
+  }
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      {contacts.map(createcontact)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -23,7 +35,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
